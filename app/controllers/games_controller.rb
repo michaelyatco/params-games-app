14d46747b875fbb51a120e517f2df6c90e4f9156
Michael Yatco <mjyatco@gmail.com>
2017-01-18 21:25:26 -0500

    Here is the form way of accepting parameters through the guess a number game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -21,6 +21,23 @@ class GamesController < ApplicationController
     render "numbers.html.erb"
   end
 
+  def guess_a_number_form_method
+    render "number_form.html.erb"
+  end
+
+  def guess_a_number_result_method
+    @guess = params[:form_message].to_i
+    @answer = 36
+      if @guess > @answer
+        @message = "Your guess is too high"
+      elsif @guess < @answer
+        @message = "Your guess is too low"
+      else
+        @message = "Your answer is correct"
+      end
+    render "guess_a_number_result.html.erb"
+  end
+
   def counter_method
     @number_of_views = params[:visit].to_i || 1
     render "counter_view.html.erb"
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   get "/name_game_url", to: "games#name_game_method"
   get "/guess_a_number_url/:this_is_the_wildcard", to: "games#guess_a_number_method" #Don't use colon in the actual link!
   get "/counter", to: "games#counter_method"
+  get "/guess_a_number_form_url", to: "games#guess_a_number_form_method"
+  post "/guess_a_number_result_url", to: "games#guess_a_number_result_method"
 end
